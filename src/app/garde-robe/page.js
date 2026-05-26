36dc9f66aba30612a98a4c9ed17c2959c2888053
Hugo <hugo.chabod@gmaiL.com>
2026-05-26 16:01:26 +0200

    fix: ccommit bug page garde-robe

---
+++ src/app/garde-robe/page.js
@@ -0,0 +1,15 @@
+'use client'
+import { useState, useEffect } from 'react'
+
+export default function GardeRobe() {
+  const [vetements, setVetements] = useState([])
+
+  useEffect(() => {
+  }, [])
+
+  return (
+    <div>
+      <h1>Ma garde-robe</h1>
+    </div>
+  )
+}
